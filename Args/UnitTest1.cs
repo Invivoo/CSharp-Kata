3c9f9235aacd6e29cf8d97c061c559777ffe6c1e
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2023-11-20 16:16:14 +0100

    Session 4 on 2323-11-16

--- Args/UnitTest1.cs
+++ Args/UnitTest1.cs
@@ -12,7 +12,7 @@ public class CmdLineParserShould
     {
         var parser = new CmdLineParser();
         var actual = parser.Parse(new string[] { "-l" });
-        Assert.True(actual.ShouldLog);
+        actual.ShouldLog.Should().Be(true);
     }
 
     [Fact]
@@ -20,7 +20,7 @@ public class CmdLineParserShould
     {
         var parser = new CmdLineParser();
         var actual = parser.Parse(new string[] { "toto" });
-        Assert.False(actual.ShouldLog);
+        actual.ShouldLog.Should().Be(false);
     }
 
     [Fact]
@@ -28,18 +28,27 @@ public class CmdLineParserShould
     {
         var parser = new CmdLineParser();
         var actual = parser.Parse(new string[] { "-p", "8080" });
-        Assert.Equal(8080, actual.Port);
+        actual.Port.Should().Be(8080);
     }
 
     [Fact]
     public void InterpretAnIntergerList()
     {
         var parser = new CmdLineParser();
-        var actual = parser.Parse(new string[] { "-d", "1,2,-3,6" });
+        var actual = parser.Parse(new string[] { "-i", "1,2,-3,6" });
         var expected = new []{1,2,-3,6};
-        Assert.Equal(expected, actual.MyNumerics);
+        actual.MyNumerics.Should().BeEquivalentTo(expected);
     }
-
+    
+    [Fact]
+    public void InterpretStringList()
+    {
+        var parser = new CmdLineParser();
+        var actual = parser.Parse(new string[] { "-g", "this,is,a,list" });
+        var expected = new[] { "this","is", "a", "list" };
+        actual.MyStrings.Should().BeEquivalentTo(expected);
+    }
+    
     [Theory]
     [MemberData(nameof(MultipleArgsParameters))]
     public void InterpretMultipleArgs(string[] args, Param expected)
@@ -67,24 +76,74 @@ public class CmdLineParserShould
         var actual = parser.Parse();
         Assert.Equal(new Param(false, 8080), actual);
     }
-
+// // 
     private static IEnumerable<object[]> MultipleArgsParameters()
     {
         yield return new object[] { new string[] { "-p", "8080", "-l" }, new Param(true, 8080) };
         yield return new object[] { new string[] { "-l", "-p", "8080" }, new Param(true, 8080) };
-        yield return new object[] { new string[] { "-l", "-p", "8080", "-d", "1,2,3" }, new Param(true, 8080, new int[]{1, 2, 3}) };
+        yield return new object[] { new string[] { "-l", "-p", "8080", "-i", "1,2,3" }, new Param(true, 8080, new int[]{1, 2, 3}) };
     }
 }
 
-public record Param(bool ShouldLog, int Port, IEnumerable<int> MyNumerics)
+public record Param(bool ShouldLog, int Port, IEnumerable<int> MyNumerics, IEnumerable<string> MyStrings)
 {
-    public Param(bool shouldLog, int port) : this(shouldLog, port, Array.Empty<int>()) { }
+    public Param(bool shouldLog, int port) : this(shouldLog, port, Array.Empty<int>(), Array.Empty<string>()) { }
+    public Param(bool shouldLog, int port, IEnumerable<int> MyNumerics) : this(shouldLog, port, MyNumerics, Array.Empty<string>()) { }
 }
+
+//public record Argument(string Key, Func<string[], int, Param, Param> ParameterBuilder, int Increment = 1);
+
+public class CChild
+{
+    public int Age { get; set; }
+}
+
+
+public abstract class Argument
+{
+    public string Key { get; init; }
+    public abstract int Increment { get; }
+
+    public Argument(string key)
+    {
+        Key = key;
+    }
+
+    public abstract Param Parse(string[] input, int position, Param parameter);
+}
+public class LogArgument : Argument
+{
+    public LogArgument() : base("-l") { }
+
+    public override int Increment => 1;
+
+    public override Param Parse(string[] input, int position, Param parameter) => parameter with { ShouldLog = true };
+}
+
+public class PortArgument : Argument
+{
+    public override int Increment => 2;
+
+    public PortArgument() : base("-l") { }
+
+    public override Param Parse(string[] input, int position, Param parameter) => parameter with { Port = ParsePort(input[position + 1]) };
+    private static int ParsePort(string port)
+    {
+        if (int.TryParse(port, out var result))
+        {
+            if (result < 0 || result > 65535)
+                throw new ArgumentException($"'{port}' is not in the expected range for a port number.");
+            return result;
+        }
+        throw new ArgumentException($"'{port}' is not a valid value for a port number.");
+    }
+}
+
 
 internal class CmdLineParser
 {
     /*Todo:
-     * Rajouter -g + support de string
+     * Refacto => remove the switch
      * Ajouter Obligatoire et facultatif
      * ...
      * Gestion d'erreur
@@ -93,17 +152,50 @@ internal class CmdLineParser
      */
     private const int DefaultParam = 8080;
 
+    //private Dictionary<string, Argument> Parsers = new()
+    //{
+    //    { "-l", new ("-l", (input, position, parameter) => parameter with { ShouldLog = true }) },
+    //    { "-p", new ("-p", (input, position, parameter) => parameter with { Port = ParsePort(input[position + 1]) }, 2) },
+    //    { "-i", new ("-i", (input, position, parameter) => parameter with { MyNumerics = ParseNumerics(input[position+ 1]) },2) },
+    //    { "-g", new ("-g", (input, position, parameter) => parameter with { MyStrings = ParseStrings(input[position+ 1]) },2) }
+    //};
+
+    //private Dictionary<string, Func<string, Param, Param>> keyValuePairs = new Dictionary<string, Func<string, Param, Param>>()
+    //{
+    //    { "-l", (input, parameter) => parameter with { ShouldLog = true } },
+    //    { "-p", (input, record) => record with { Port = ParsePort(args[++index]) } },
+    //    { "-i", (input, record) => record with { MyNumerics = ParseNumerics(args[++index])},
+    //    { "-g", (input, record) => record with { MyStrings = ParseStrings(args[++index]) }
+    //};
+
+    //private static readonly Argument[] SupportedArguments = new
+    //{
+
+    //};
+
     internal Param Parse(params string[] args)
     {
         var record = new Param(false, DefaultParam);
         for (var index = 0; index < args.Length; index++)
         {
-            if (args[index] == "-l")
-                record = record with { ShouldLog = true };
-            if (args[index] == "-p")
-                record = record with { Port = ParsePort(args[++index]) };
-            if (args[index] == "-d")
-                record = record with { MyNumerics = ParseNumerics(args[++index]).ToArray()};
+            switch(args[index])
+            {
+                case "-l":
+                    record = record with { ShouldLog = true };
+                    break;
+                case "-p":
+                    record = record with { Port = ParsePort(args[++index]) };
+                    break;
+                case "-i":
+                    record = record with { MyNumerics = ParseNumerics(args[++index])};
+                    break;
+                case "-g":
+                    record = record with { MyStrings = ParseStrings(args[++index]) };
+                    break;
+                default:
+                    continue;
+                    //throw new ArgumentException($"Unrecognize argument: {args[index]}");
+            }
         }
         return record;
     }
@@ -120,4 +212,5 @@ internal class CmdLineParser
     }
 
     private static IEnumerable<int> ParseNumerics(string numerics) => numerics.Split(',').Select(x => int.Parse(x));
+    private static IEnumerable<string> ParseStrings(string strings) => strings.Split(',');
 }
